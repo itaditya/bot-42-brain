3b0e87366a4d754668f12b662392774f35b864b0
Aditya Agarwal <adityaa803@gmail.com>
2018-01-24 11:18:13 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,11 @@
+# BOT-42 Brain
+
+> This is the brain of bot-42 (my automation bot) coded in Node
+
+## How To Run ???
+
+  * Clone the repo and `cd` into it.
+  * In `tutorial.js` fill in the constants.
+  * Run `node tutorial.js "commit_msg" "file_content"`
+
+    Example -- `node index.js "trial commit" "automation is wonderful"`.
--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const TOKEN = process.env.TOKEN;
 
 (async() => {
   // Create file
-  const COMMIT_MSG = process.argv[2]
-  const FILE_CONTENT = process.argv[3]
+  const COMMIT_MSG = process.argv[2];
+  const FILE_CONTENT = process.argv[3];
 
   const OWNER = 'itaditya';
   const REPO_NAME = 'test-for-my-gitbot';
   const FILE_PATH = 'folder-2/test5.txt';
 
-  const API_URL = `${BASE_URL}/repos/${OWNER}/${REPO_NAME}/contents/${FILE_PATH}`
+  const API_URL = `${BASE_URL}/repos/${OWNER}/${REPO_NAME}/contents/${FILE_PATH}`;
   try {
     const res = await request
       .put(API_URL)
@@ -22,10 +22,10 @@ const TOKEN = process.env.TOKEN;
         message: COMMIT_MSG,
         content: base64.encode(FILE_CONTENT)
       })
-      .set('Authorization', `token ${TOKEN}`)
+      .set('Authorization', `token ${TOKEN}`);
     console.log('File Created, see: ', res.body.content.html_url);
   } catch (err) {
-    console.log('OOPS !! Some Error Occured ....')
+    console.log('OOPS !! Some Error Occured ....');
     console.log('err', err);
   }
 })()
